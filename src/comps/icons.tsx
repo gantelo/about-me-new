6255cf5250c202fe7f802e6f6c4d4d4bb1de8513
Gabriel Antelo <gabriel.antelo@southworks.com>
2022-08-17 11:14:36 -0300

    🔥 Remove old page about me

--- src/comps/icons.tsx
+++ src/comps/icons.tsx
@@ -2,7 +2,6 @@ import React, { memo } from "react";
 import { ReactComponent as LinkedIn } from "../assets/linkedin.svg";
 import { ReactComponent as Code } from "../assets/code.svg";
 import { ReactComponent as Profile } from "../assets/profile.svg";
-import { ReactComponent as Game } from "../assets/game.svg";
 import Icon from "./icon";
 import "../App.css";
 
@@ -23,13 +22,6 @@ const Icons = () => {
         Element={Code}
         onClick={() => window.open("https://github.com/gantelo", "_blank")}
         text="GitHub"
-      />
-      <Icon
-        Element={Game}
-        onClick={() =>
-          window.open("https://gantelo.github.io/about-me/", "_blank")
-        }
-        text="Showcase"
       />
       <Icon
         Element={Profile}
